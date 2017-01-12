5a60238e788874d4ecee2b219d138eccedba434f
rbadr <rahal.badr@gmail.com>
2017-01-12 21:44:45 +0100

    use variables to extract data from the received messages

--- index.js
+++ index.js
@@ -73,27 +73,40 @@ app.post('/webhook', function(req, res) {
             } else if (values.indexOf("build") >= 0 && values.indexOf("project") >= 0 && values.indexOf("informations") >= 0) {
                 sendMessage(event.sender.id, { text: "Yes for sure, I'll just need your repo's name as followed repoOwner : repoName" });
             } else if (values.indexOf(":") >= 0) {
-                sendMessage(event.sender.id, { text: "The last build status for this project is : Passed." });
+                var repoOwner = values[values.indexOf(":") - 1];
+                var repoName = values[values.indexOf(":") + 1];
+                var buildStatus = "Passed";
+                sendMessage(event.sender.id, { text: "The last build status for " + repoOwner + "/" + repoName + " is : " + buildStatus });
             } else if (values.indexOf("build") >= 0 && values.indexOf("id") >= 0) {
-                sendMessage(event.sender.id, { text: "The build id is : 22555277" });
+                var buildId = "22555277";
+                sendMessage(event.sender.id, { text: "The build id is : " + buildId });
             } else if (values.indexOf("build") >= 0 && values.indexOf("number") >= 0) {
-                sendMessage(event.sender.id, { text: "The build number is : 784" });
+                var buildNumber = "784";
+                sendMessage(event.sender.id, { text: "The build number is : " + buildNumber });
             } else if (values.indexOf("who") >= 0 && values.indexOf("commited") >= 0) {
-                sendMessage(event.sender.id, { text: "The last person to commit and trigger the build is : Rahal Badr" });
+                var authorName = "Rahal Badr";
+                sendMessage(event.sender.id, { text: "The last person to commit and trigger the build is : " + authorName });
             } else if (values.indexOf("commit") >= 0 && values.indexOf("id") >= 0) {
-                sendMessage(event.sender.id, { text: "The commit id of this build is : 6534711" });
+                var commitId = "6534711";
+                sendMessage(event.sender.id, { text: "The commit id of this build is : " + commitId });
             } else if (values.indexOf("build") >= 0 && values.indexOf("started") >= 0) {
-                sendMessage(event.sender.id, { text: "The time the build was started : 2014-04-08T19:37:44Z" });
+                var startedAt = "2014-04-08T19:37:44Z";
+                sendMessage(event.sender.id, { text: "The time the build was started : " + startedAt });
             } else if (values.indexOf("build") >= 0 && values.indexOf("finished") >= 0) {
-                sendMessage(event.sender.id, { text: "The time the build finished : 2014-04-08T19:52:56Z" });
+                var finishedAt = "2014-04-08T19:37:44Z";
+                sendMessage(event.sender.id, { text: "The time the build finished : " + finishedAt });
             } else if (values.indexOf("duration") >= 0) {
-                sendMessage(event.sender.id, { text: "The build duration was : 2648" });
+                var buildDuration = "2648";
+                sendMessage(event.sender.id, { text: "The build duration was : " + buildDuration });
             } else if (values.indexOf("pull") >= 0 && values.indexOf("request") >= 0 && values.indexOf("title") >= 0) {
-                sendMessage(event.sender.id, { text: "Example PR" });
+                var pullRequestTitle = "Example PR";
+                sendMessage(event.sender.id, { text: pullRequestTitle });
             } else if (values.indexOf("pull") >= 0 && values.indexOf("request") >= 0 && values.indexOf("number") >= 0) {
-                sendMessage(event.sender.id, { text: "The PR number is : 1912" });
+                var pullRequestNumber = "1912";
+                sendMessage(event.sender.id, { text: "The PR number is : " + pullRequestNumber });
             } else if (values.indexOf("pull") >= 0 && values.indexOf("request") >= 0) {
-                sendMessage(event.sender.id, { text: "True" });
+                var isPullRequest = "True";
+                sendMessage(event.sender.id, { text: isPullRequest });
             } else if (values.indexOf("Thanks") >= 0) {
                 sendMessage(event.sender.id, { text: "You're welcome ;)" });
             } else {
