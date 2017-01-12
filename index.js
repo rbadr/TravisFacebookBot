6650eeff8c3d438b1e39aeaccdb5a17d401e83ff
rbadr <rahal.badr@gmail.com>
2017-01-12 20:09:17 +0100

    refactoring + add new keywords

--- index.js
+++ index.js
@@ -3,15 +3,13 @@ var bodyParser = require('body-parser');
 var request = require('request');
 var app = express();
 
-var facebook_user_name = "";
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.listen((process.env.PORT || 3000));
 
 // Server frontpage
 app.get('/', function(req, res) {
-    res.send('This is TestBot Server');
+    res.send('This is the CI Notifier Server, a facebook messeger bot that let you interact with Travis CI.');
 });
 
 // Facebook Webhook
@@ -23,32 +21,7 @@ app.get('/webhook', function(req, res) {
     }
 });
 
-// handler receiving messages
-app.post('/webhook', function(req, res) {
-    var events = req.body.entry[0].messaging;
-    for (i = 0; i < events.length; i++) {
-        var event = events[i];
-        if (event.message && event.message.text) {
-            var values = event.message.text.split(' ');
-            userInfoRequest(event.sender.id)
-                .then((userInfo) => {
-
-                    facebook_user_name: userInfo.first_name
-                });
-
-            if (values[0] === 'Hello') {
-                sendMessage(event.sender.id, { text: "Hello " + facebook_user_name + ",How may I help you ?" })
-            } else if (!kittenMessage(event.sender.id, event.message.text)) {
-                sendMessage(event.sender.id, { text: "Echo: " + event.message.text });
-            }
-        } else if (event.postback) {
-            console.log("Postback received: " + JSON.stringify(event.postback));
-        }
-    }
-    res.sendStatus(200);
-});
-
-// generic function sending messages
+// The main generic function sending messages
 function sendMessage(recipientId, message) {
     request({
         url: 'https://graph.facebook.com/v2.8/me/messages',
@@ -67,50 +40,7 @@ function sendMessage(recipientId, message) {
     });
 };
 
-// send rich message with kitten
-function kittenMessage(recipientId, text) {
-
-    text = text || "";
-    var values = text.split(' ');
-
-    if (values.length === 3 && values[0] === 'kitten') {
-        if (Number(values[1]) > 0 && Number(values[2]) > 0) {
-
-            var imageUrl = "https://placekitten.com/" + Number(values[1]) + "/" + Number(values[2]);
-
-            message = {
-                "attachment": {
-                    "type": "template",
-                    "payload": {
-                        "template_type": "generic",
-                        "elements": [{
-                            "title": "Kitten",
-                            "subtitle": "Cute kitten picture",
-                            "image_url": imageUrl,
-                            "buttons": [{
-                                "type": "web_url",
-                                "url": imageUrl,
-                                "title": "Show kitten"
-                            }, {
-                                "type": "postback",
-                                "title": "I like this",
-                                "payload": "User " + recipientId + " likes kitten " + imageUrl,
-                            }]
-                        }]
-                    }
-                }
-            };
-
-            sendMessage(recipientId, message);
-
-            return true;
-        }
-    }
-
-    return false;
-
-};
-
+// Get the user first name and last name using his id 
 function userInfoRequest(userId) {
     return new Promise((resolve, reject) => {
         request({
@@ -128,3 +58,50 @@ function userInfoRequest(userId) {
             });
     });
 };
+
+
+
+// handler receiving messages
+app.post('/webhook', function(req, res) {
+    var events = req.body.entry[0].messaging;
+    for (i = 0; i < events.length; i++) {
+        var event = events[i];
+        if (event.message && event.message.text) {
+            var values = event.message.text.split(' ');
+            if (values.indexOf("Hello") >= 0) {
+                sendMessage(event.sender.id, { text: "Hello, How may I help you ?" });
+            } else if (values.indexOf("build") >= 0 && values.indexOf("project") >= 0 && values.indexOf("informations") >= 0) {
+                sendMessage(event.sender.id, { text: "Yes for sure, I'll just need your repo's name as followed repoOwner : repoName" });
+            } else if (values.indexOf(":") >= 0) {
+                sendMessage(event.sender.id, { text: "The last build status for this project is : Passed." });
+            } else if (values.indexOf("build") >= 0 && values.indexOf("id") >= 0) {
+                sendMessage(event.sender.id, { text: "The build id is : 22555277" });
+            } else if (values.indexOf("build") >= 0 && values.indexOf("number") >= 0) {
+                sendMessage(event.sender.id, { text: "The build number is : 784" });
+            } else if (values.indexOf("who") >= 0 && values.indexOf("commited") >= 0) {
+                sendMessage(event.sender.id, { text: "The last person to commit and trigger the build is : Rahal Badr" });
+            } else if (values.indexOf("commit") >= 0 && values.indexOf("id") >= 0) {
+                sendMessage(event.sender.id, { text: "The commit id of this build is : 6534711" });
+            } else if (values.indexOf("build") >= 0 && values.indexOf("started") >= 0) {
+                sendMessage(event.sender.id, { text: "The time the build was started : 2014-04-08T19:37:44Z" });
+            } else if (values.indexOf("build") >= 0 && values.indexOf("finished") >= 0) {
+                sendMessage(event.sender.id, { text: "The time the build finished : 2014-04-08T19:52:56Z" });
+            } else if (values.indexOf("duration") >= 0) {
+                sendMessage(event.sender.id, { text: "The build duration was : 2648" });
+            } else if (values.indexOf("pull") >= 0 && values.indexOf("request") >= 0) {
+                sendMessage(event.sender.id, { text: "True" });
+            } else if (values.indexOf("pull") >= 0 && values.indexOf("request") >= 0 && values.indexOf("title") >= 0) {
+                sendMessage(event.sender.id, { text: "Example PR" });
+            } else if (values.indexOf("pull") >= 0 && values.indexOf("request") >= 0 && values.indexOf("number") >= 0) {
+                sendMessage(event.sender.id, { text: "The PR number is : 1912" });
+            } else if (values.indexOf("Thanks") >= 0) {
+                sendMessage(event.sender.id, { text: "You're welcome ;)" });
+            } else {
+                sendMessage(event.sender.id, { text: "Sorry I didn't undersant what you meant :( Try me again !" });
+            }
+        } else if (event.postback) {
+            console.log("Postback received: " + JSON.stringify(event.postback));
+        }
+    }
+    res.sendStatus(200);
+});
